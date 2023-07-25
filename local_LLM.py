64feddc62f13d2e3658a5f4c804d4f3228e22f17
Ha0e <122515100+pphhyy0620@users.noreply.github.com>
2023-07-25 20:54:24 +0900

    Update local_LLM.py

--- local_LLM.py
+++ local_LLM.py
@@ -1,3 +1,6 @@
+# This code is based on [localGPT], originally created by [PromptEngineer].
+# Modified by pphhyy0620 under the terms of the Apache License, Version 2.0.
+
 import logging
 
 import click
